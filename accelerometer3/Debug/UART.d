0aac9705052e97001ebf1bbf80dd2f818722bffd
pacific_ecpe170 <kelvinflores@dhcp-10-11-195-38.wireless.pacific.edu>
2016-10-10 11:17:40 -0700

    Added initial Files w/ trash

---
+++ accelerometer3/Debug/UART.d
@@ -0,0 +1,31 @@
+# FIXED
+
+UART.obj: ../UART.c
+UART.obj: ../UART.h
+UART.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdio.h
+UART.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h
+UART.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdarg.h
+UART.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
+UART.obj: /Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/gpio.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/pin_map.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/uart.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/sysctl.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/inc/tm4c123gh6pm.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/inc/hw_memmap.h
+UART.obj: /Users/kelvinflores/Documents/TIVA/TivaWare/inc/hw_sysctl.h
+
+../UART.c: 
+../UART.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdio.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdarg.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
+/Applications/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/gpio.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/pin_map.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/uart.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/sysctl.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/inc/tm4c123gh6pm.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/inc/hw_memmap.h: 
+/Users/kelvinflores/Documents/TIVA/TivaWare/inc/hw_sysctl.h: 
---
+++ accelerometer3/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"/Users/kelvinflores/Documents/TIVA/TivaWare/driverlib/ccs/Debug/driverlib.lib" -llibc.a
+
